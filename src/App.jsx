a88176445e3aed9a08c2cd550381c8ad6b6a5e7e
salkesakshant@gmail.com <salkesakshant@gmail.com>
2026-06-27 17:02:13 +0530

    imported react router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/shared/Header'
+import { Route ,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateTrip from './pages/CreateTrip'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/create-trip' element={<CreateTrip />}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
