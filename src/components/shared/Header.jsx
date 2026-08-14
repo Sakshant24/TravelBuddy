e09a8cb612633e9eee513f00a98632b1c26b23ed
salkesakshant@gmail.com <salkesakshant@gmail.com>
2026-08-14 12:26:24 +0530

    header updated

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,21 @@
 import { Plane, Plus, User } from 'lucide-react'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 import LoginDialog from './LoginDialog'
+import { googleLogout } from '@react-oauth/google'
 
 const Header = () => {
     const [openDialog, setOpenDialog] = useState(false)
+    const user = JSON.parse(localStorage.getItem("user"))
+    const navigate = useNavigate()
+
+    const handleLogout = ()=>{
+        googleLogout()
+        localStorage.removeItem("user")
+        navigate('/')
+    }
+
   return (
     <header className='bg-white border-b border-gray-200 px-6 py-3 flexBetween absolute top-0 left-0 right-0 w-full z-50'>
         {/* Logo */}
@@ -22,10 +32,14 @@ const Header = () => {
                Create Trip 
             </Button>
             <div className='flex mt-1'>
-                <Button onClick={()=> setOpenDialog(true)} className="bg-indigo-600 hover:bg-indigo-700 text-white px-5!">
-                <User />
-                Login 
-                </Button>
+                { user ? <div>
+                        <img src={user?.picture} alt='userProfile' height={37} width={37} className='rounded-full'/>
+                    </div> : 
+                        <Button onClick={()=> setOpenDialog(true)} className="bg-indigo-600 hover:bg-indigo-700 text-white px-5!">
+                            <User />
+                            Login 
+                        </Button>
+                }
                 <LoginDialog open={openDialog} onClose={()=>setOpenDialog(false)}/>
             </div>
         </div>
